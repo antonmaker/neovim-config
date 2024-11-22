9982f12e935af13f42f84f93d21393421038cfad
Anton Emelyanov <antonkham@gmail.com>
2024-11-22 11:33:15 +0900

    Update LSPs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,10 +17,23 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = {
+      { "ms-jpq/coq_nvim", branch = "coq" },
+      { "ms-jpq/coq.artifacts", branch = "artifacts" },
+      { 'ms-jpq/coq.thirdparty', branch = "3p" },
+    },
+    init = function()
+      vim.g.coq_settings = {
+        auto_start = true,
+      }
+    end,
 		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.ts_ls.setup({})
+			local lsp = require "lspconfig"
+      local coq = require "coq"
+
+			lsp.lua_ls.setup(coq.lsp_ensure_capabilities({}))
+			lsp.ts_ls.setup(coq.lsp_ensure_capabilities({}))
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
