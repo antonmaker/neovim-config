d536484119fc2a68151ca48d0d95bbe97b7e9f5c
Anton Emelyanov <antonkham@gmail.com>
2024-12-06 15:47:50 +0900

    add max col line

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set colorcolumn=80")
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
